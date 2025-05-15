d1fd31e85a4e15d337968f19d9a68b347806c7a9
Tutitoos <tutitoos.xyz@gmail.com>
2025-05-15 19:16:20 +0200

    Add nil check for notifier in UpdateNotifiers function

--- types/services/notifications.go
+++ types/services/notifications.go
@@ -14,6 +14,10 @@ func AddNotifier(n ServiceNotifier) {
 func UpdateNotifiers() {
 	for _, n := range notifications.All() {
 		notifier := allNotifiers[n.Method]
+		if notifier == nil {
+			// Optionally log a warning here
+			continue
+		}
 		notifier.Select().UpdateFields(n)
 	}
 }
