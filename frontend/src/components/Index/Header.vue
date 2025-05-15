2039cbb07dc1f996d86aa50cd52283a9dc132198
Tutitoos <tutitoos.xyz@gmail.com>
2025-05-16 01:48:44 +0200

    Add router link for dashboard/login navigation in Header component

--- frontend/src/components/Index/Header.vue
+++ frontend/src/components/Index/Header.vue
@@ -3,6 +3,9 @@
         <h1 id="title" class="col-12 text-center pt-4 mt-4 mb-3 header-title font-6">{{core.name}}</h1>
         <h5 id="description" class="col-12 text-center mb-5 header-desc font-3">{{core.description}}</h5>
     </div>
+    <div class="mb-3">
+      <router-link class="links" :to="admin ? '/dashboard' : '/login'">{{$t('dashboard')}}</router-link>
+    </div>
 </template>
 
 <script>
