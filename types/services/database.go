c53ccef97a25e41eb3e136843bf30f0f946ea3ab
Tutitoos <tutitoos.xyz@gmail.com>
2025-06-05 20:36:12 +0200

    Refactor Find function to remove legacy ID lookup code and streamline service retrieval logic

--- types/services/database.go
+++ types/services/database.go
@@ -69,15 +69,6 @@ func SetDB(database database.Database) {
 	db = database.Model(&Service{})
 }
 
-// func Find(id int64) (*Service, error) {
-// 	service := allServices[id]
-// 	if service == nil {
-// 		return nil, errors.Missing(&Service{}, id)
-// 	}
-
-// 	return service, nil
-// }
-
 func Find(id int64, permalink ...string) (*Service, error) {
 	service := &Service{}
 
@@ -98,16 +89,6 @@ func Find(id int64, permalink ...string) (*Service, error) {
 	}
 
 	return service, nil
-
-	// // Try to find by ID first
-	// q := db.Where("id = ?", id).Find(&service)
-
-	// // Try permalink if ID search had an error or found nothing, and permalink is provided
-	// if (q.Error() != nil || q.RowsAffected() == 0) && len(permalink) > 0 && permalink[0] != "" {
-	// 	q = db.Where("permalink = ?", permalink[0]).Find(&service)
-	// }
-
-	// return service, q.Error()
 }
 
 func all() []*Service {
